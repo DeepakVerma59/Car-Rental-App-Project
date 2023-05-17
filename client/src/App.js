16eba50c932ce4249dd896e74956ec6111fd7864
ani20101999 <rishurout20@gmail.com>
2023-05-17 15:29:43 +0530

    changes made in routes,app.js,controller

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,10 @@ import { Route, Routes } from 'react-router-dom';
 import Register from './pages/authPages/Register';
 import Login from './pages/authPages/Login';
 import Forgotpassword from './pages/auth-pages/forgotpassword';
+import AdminHomePage from './pages/admin/adminHomePage';
+import AddProducts from './pages/admin/addProducts';
+import PageNotFound from './pages/PageNotFound';
+import EditProducts from './pages/admin/EditProducts';
 
 function App() {
   return <>
@@ -11,6 +15,10 @@ function App() {
     <Route path='/register' element={<Register/>}/>
     <Route path='/login' element={<Login/>}/>
     <Route path='/forgot-password' element={<Forgotpassword/>}/>
+    <Route path='/admin-homepage' element={<AdminHomePage/>}/>
+    <Route path='/admin-addproducts' element={<AddProducts/>}/>
+    <Route path='/admin-editproducts' element={<EditProducts/>}/>
+    <Route path='*' element={<PageNotFound/>}/>
   </Routes>
   </>
 
---
+++ server/routes/productRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { requireSignin, isAdmin } = require("../middlewares/authMiddleware");
+const formidable = require("express-formidable")
+const route = express.Router();
+const {createProductController,updateProductController,getProductController,getPhotoController,deleteProductController} = require("../controllers/productController")
+
+
+//create product
+route.post("/create-product",requireSignin,isAdmin,formidable(),createProductController);
+
+//update product
+route.put("/update-product/:pid",requireSignin,isAdmin,formidable(),updateProductController);
+
+//get products
+route.get("/get-product",getProductController);
+
+//get-photo
+route.get("/get-photo/:pid",getPhotoController);
+
+//delete-product
+route.delete("/delete-product/:id",requireSignin,isAdmin,deleteProductController)
+
+module.exports = route

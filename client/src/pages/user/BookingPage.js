e7c3e3a7ec5dfcc37e4cac76fa4cae45935c48bc
monalisawant <monalisawant1999@gmail.com>
2023-05-20 01:38:47 +0530

    booking page updated

--- client/src/pages/user/BookingPage.js
+++ client/src/pages/user/BookingPage.js
@@ -1,75 +1,92 @@
 import React from 'react'
 import "../styles/BookingDetails.css"
 import UserHeader from './userHeader'
-import OrderCarHeader from './orderpageheader/OrderPageHeader'
+import car from "../styles/images/booking page car demo.png"
+import map from "../styles/images/map demo.png"
+import { Link } from 'react-router-dom'
+
 
 
 function BookingPage() {
   return (
     <>
     <UserHeader/>
-    <OrderCarHeader/>
+    
     <div>
 <div className="card  card1 ">
   <div className="card-body mx-5" >
     <h5 className="card-title">Booking details</h5>
   
     <form >
-  <div className="form-group row mt-4 ">
-    <label for="carname" className="col-sm-2 col-form-label">Car name</label>
-    <div className="col-sm-5">
-      <input type="text"  className="form-control " id="carname" value=""/>
+<div className='text'>
+  <div>
+  <div className="form-group row mt-4 text ">
+    <label for="carname" className="col-sm-4 col-form-label">Car name</label>
+    <div className="col-sm-8">
+      <input type="text"  className="form-control-plaintext " id="carname" value=""/>
     </div>
+ 
+   
   </div>
   <div className="form-group row mt-4">
-    <label for="CarNumber" className="col-sm-2 col-form-label">Car Number</label>
-    <div className="col-sm-5">
-      <input type="text" className="form-control" id="CarNumber" value=""/>
+    <label for="CarNumber" className="col-sm-4 col-form-label">Car Number</label>
+    <div className="col-sm-8 ">
+      <input type="text" className="form-control-plaintext px-4" id="CarNumber" placeholder="KA 37 BB 9999"/>
+    </div>
     </div>
   </div>
+  <div className='democar'><img className="democar" src={car} alt="car" />  <Link to="/order-car"> <span class="bi bi-pencil style='color: black;'"> </span></Link></div>
+  </div>
   <hr/>
+
+
+<div className='text'>
+<div>
   <div className="form-group row mt-2">
-    <label for="origin" className="col-sm-2 col-form-label">Origin</label>
-    <div className="col-sm-5">
-      <input type="text"  className="form-control" id="origin" value=""/>
+    <label for="origin" className="col-sm-4 col-form-label">Origin</label>
+    <div className="col-sm-8">
+      <input type="text"  className="form-control-plaintext" id="origin" value=""/>
     </div>
     </div>
 
   <div className="form-group row mt-2">
-    <label for="destination" className="col-sm-2 col-form-label">Destination</label>
-    <div className="col-sm-5">
-      <input type="text"  className="form-control" id="destination" value=""/>
+    <label for="destination" className="col-sm-5 col-form-label">Destination</label>
+    <div className="col-sm-6">
+      <input type="text"  className="form-control-plaintext" id="destination" value=""/>
     </div>
   </div>
   <div className="form-group row mt-2">
-    <label for="startdate" className="col-sm-2 col-form-label">Start Date</label>
-    <div className="col-sm-5">
-      <input type="text"  className="form-control" id="startdate" value=""/>
+    <label for="startdate" className="col-sm-4 col-form-label">Start Date</label>
+    <div className="col-sm-6">
+      <input type="text"  className="form-control-plaintext" id="startdate" value=""/>
     </div>
   </div>
   <div className="form-group row mt-2">
-    <label for="enddate" className="col-sm-2 col-form-label">End Date</label>
-    <div className="col-sm-5">
-      <input type="text"  className="form-control" id="enddate" value=""/>
+    <label for="enddate" className="col-sm-4 col-form-label">End Date</label>
+    <div className="col-sm-6">
+      <input type="text"  className="form-control-plaintext" id="enddate" value=""/>
     </div>
+  </div>
+</div>
+  <div className='democar'><img className="democar" src={map} alt="car" /><Link to="/order-car"> <span class="bi bi-pencil style='color: black;'"> </span></Link></div>
   </div>
   <hr/>
   <div className="form-group row mt-2">
-    <label for="bookingid" className="col-sm-2 col-form-label">Booking ID</label>
-    <div className="col-sm-5">
-      <input type="text"  className="form-control" id="bookingid" value="GOTAP6"/>
+    <label for="bookingid" className="col-sm-4 col-form-label">Booking ID</label>
+    <div className="col-sm-6">
+      <input type="text"  className="form-control-plaintext" id="bookingid" value="GOTAP6"/>
     </div>
   </div>
   <div className="form-group row mt-2">
-    <label for="btime" className="col-sm-2 col-form-label">Booking Time</label>
-    <div className="col-sm-5">
-      <input type="text"  className="form-control" id="btime" value=""/>
+    <label for="btime" className="col-sm-4 col-form-label">Booking Time</label>
+    <div className="col-sm-6">
+      <input type="text"  className="form-control-plaintext" id="btime" value=""/>
     </div>
   </div>
   <div className="form-group row mt-2">
-    <label for="bdate" className="col-sm-2 col-form-label">Booking Date</label>
-    <div className="col-sm-5">
-      <input type="text"  className="form-control" id="bdate" value=""/>
+    <label for="bdate" className="col-sm-4 col-form-label">Booking Date</label>
+    <div className="col-sm-6">
+      <input type="text"  className="form-control-plaintext" id="bdate" value=""/>
     </div>
   </div>
   
@@ -91,28 +108,28 @@ function BookingPage() {
   <div className="form-group row mt-4 ">
     <label for="Priceperkm" className="col-sm-5 col-form-label">Price perkm</label>
     <div className="col-sm-5">
-      <input type="text"  className="form-control " id="Priceperkm" value=""/>
+      <input type="text"  className="form-control-plaintext " id="Priceperkm" value=""/>
     </div>
   </div>
   
   <div className="form-group row mt-2">
     <label for="pricing" className="col-sm-5 col-form-label">Pricing</label>
     <div className="col-sm-5">
-      <input type="text" className="form-control" id="pricing" value=""/>
+      <input type="text" className="form-control-plaintext" id="pricing" value=""/>
     </div>
   </div>
   
   <div className="form-group row mt-2">
     <label for="taxcharge" className="col-sm-5 col-form-label">Tax charges</label>
     <div className="col-sm-5">
-      <input type="text"  className="form-control" id="taxcharge" value=""/>
+      <input type="text"  className="form-control-plaintext" id="taxcharge" value=""/>
     </div>
   </div>
   <hr/>
   <div className="form-group row mt-2">
     <label for="subTotal" className="col-sm-5 col-form-label">Sub Total</label>
     <div className="col-sm-5">
-      <input type="text"  className="form-control" id="subTotal" value=""/>
+      <input type="text"  className="form-control-plaintext" id="subTotal" value=""/>
     </div>
   </div>
 <br/>
--- client/src/pages/user/orderpageheader/OrderPageHeader.js
+++ client/src/pages/user/orderpageheader/OrderPageHeader.js
@@ -11,21 +11,23 @@ function OrderPageHeader() {
         <form id="form" action="">
 
           <li className='Origin'
-          >Origin :{orderHeader.origin}</li>
+          >O{orderHeader.origin}
 
-          <i className="fa-solid fa-arrow-right  arrow" style={{ color: "#4279cd" }}></i>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+        </svg>
 
-          <li className="Origin"
-          >Destination :{orderHeader.destination}</li>
-
-          <li type="date" className="Origin"
-          >Starting from :{orderHeader.startDate}</li>
+           D{orderHeader.destination}</li>
 
           <li type="date" className="Origin"
-          >Ending :{orderHeader.endDate}</li>
+          >S{orderHeader.startDate} 
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+             </svg>
+          E{orderHeader.endDate}</li>
 
 
-          <Link to="/booking-check" id="modify" onClick={''}>Edit</Link>
+          <Link to="/booking-check" id="modify" onClick={''}>MODIFY</Link>
         </form>
 
       </div>
